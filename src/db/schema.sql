682b70dc0b87baf7c188f41f54668ec7e8de8b28
Gregor Aisch <gka@users.noreply.github.com>
2017-10-08 23:48:04 +0200

    no more test data

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -18,22 +18,3 @@ CREATE TABLE user (
     blocked BOOLEAN,
     trusted BOOLEAN
 );
-
--- test data
--- INSERT INTO user(name, created_at, trusted, blocked)
---     VALUES
---         ("Normal User",datetime(), 0, 0),
---         ("Trusted User",datetime(), 1, 0),
---         ("Blocked User",datetime(), 0, 1);
-
--- INSERT INTO comment(user_id, slug, created_at, comment, approved, rejected)
---     VALUES
---         (1,"foo", datetime(), "This comment was written by a normal user and was approved", 1, 0),
---         (1,"foo", datetime(), "This comment was written by a normal user and was rejected", 0, 1),
---         (1,"foo", datetime(), "This comment was written by a normal user and was neither approved nor rejected", 0, 0),
---         (2,"foo", datetime(), "This comment was written by a trusted user and was approved", 1, 0),
---         (2,"foo", datetime(), "This comment was written by a trusted user and was rejected", 0, 1),
---         (2,"foo", datetime(), "This comment was written by a trusted user and was neither approved nor rejected", 0, 0),
---         (3,"foo", datetime(), "This comment was written by a blocked user and was approved", 1, 0),
---         (3,"foo", datetime(), "This comment was written by a blocked user and was rejected", 0, 1),
---         (3,"foo", datetime(), "This comment was written by a blocked user and was neither approved nor rejected", 0, 0);
