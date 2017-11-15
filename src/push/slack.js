75f713697476e950ef11520ed431f392382bcd66
Gregor Aisch <gka@users.noreply.github.com>
2017-11-15 16:48:08 +0100

    include slug in slack msg

--- src/push/slack.js
+++ src/push/slack.js
@@ -7,7 +7,7 @@ if (config.notify.slack) {
     schnackEvents.on('new-comment', (event) => {
         const post_url = config.page_url.replace('%SLUG%', event.slug)+'#comment-'+event.id;
         const comment = event.comment.split(/\n+/).map(s => s ? `> _${s}_` : '>').join('\n>\n');
-        const text = `A <${post_url}|new comment> was posted by ${event.user.display_name || event.user.name}:\n\n${comment}`;
+        const text = `A <${post_url}|new comment> was posted by ${event.user.display_name || event.user.name} under *${event.slug}*:\n\n${comment}`;
         request({
             url: config.notify.slack.webhook_url,
             method: 'post',
