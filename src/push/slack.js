f8363850026858ff20310532bc4a5e3c11060bf2
Jan Piotrowski <piotrowski@gmail.com>
2017-12-19 19:09:27 +0100

    Fix: unchanged default `notify.slack.webhook_url` (xxxxx) doesn't crash app any more

--- src/push/slack.js
+++ src/push/slack.js
@@ -3,7 +3,7 @@ const request = require('request');
 const config = require('../../config.json');
 const schnackEvents = require('../events');
 
-if (config.notify.slack) {
+if (config.notify.slack && config.notify.slack.webhook_url != 'xxxxx') {
     schnackEvents.on('new-comment', (event) => {
         const post_url = config.page_url.replace('%SLUG%', event.slug)+'#comment-'+event.id;
         const comment = event.comment.split(/\n+/).map(s => s ? `> _${s}_` : '>').join('\n>\n');
