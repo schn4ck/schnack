eedb6c59892b3400dca74f71b812c68f754be70c
g-div <g-div@anche.no>
2018-01-05 15:17:52 +0100

    fix(slack): add try/catch

--- src/push/slack.js
+++ src/push/slack.js
@@ -5,14 +5,18 @@ const schnackEvents = require('../events');
 
 if (config.notify.slack) {
     schnackEvents.on('new-comment', (event) => {
-        const post_url = config.page_url.replace('%SLUG%', event.slug)+'#comment-'+event.id;
-        const comment = event.comment.split(/\n+/).map(s => s ? `> _${s}_` : '>').join('\n>\n');
-        const text = `A <${post_url}|new comment> was posted by ${event.user.display_name || event.user.name} under *${event.slug}*:\n\n${comment}`;
-        request({
-            url: config.notify.slack.webhook_url,
-            method: 'post',
-            json: true,
-            body: { text }
-        });
+        try {
+            const post_url = config.page_url.replace('%SLUG%', event.slug)+'#comment-'+event.id;
+            const comment = event.comment.split(/\n+/).map(s => s ? `> _${s}_` : '>').join('\n>\n');
+            const text = `A <${post_url}|new comment> was posted by ${event.user.display_name || event.user.name} under *${event.slug}*:\n\n${comment}`;
+            request({
+                    url: config.notify.slack.webhook_url,
+                    method: 'post',
+                    json: true,
+                    body: { text }
+            });
+        } catch (error) {
+            console.error('Error sending slack notification:', error);
+        }
     });
 }
