184c2aefed5884d574f056b9e57af5b7f8652515
Gregor Aisch <gka@users.noreply.github.com>
2021-01-15 18:15:57 +0100

    fix db handler

--- src/db/index.js
+++ src/db/index.js
@@ -5,21 +5,20 @@ const config = require('../config');
 const conf = config.get('database');
 
 // returns promise that passes db obj
-function init() {
-    const dbname = conf.comments || conf;
-    const dbpath = path.resolve(process.cwd(), dbname);
-
-    return Promise.resolve(open({ filename: dbpath, driver: sqlite3.Database }))
-        .then(db =>
-            db.migrate({
-                migrationsPath: path.resolve(__dirname, '../../migrations'),
-                // force: process.env.NODE_ENV === 'development' ? 'last' : false
-                force: false
-            })
-        )
-        .catch(err => console.error(err));
-}
-
 module.exports = {
-    init
+    init() {
+        const dbname = conf.comments || conf;
+        const dbpath = path.resolve(process.cwd(), dbname);
+
+        return Promise.resolve(open({ filename: dbpath, driver: sqlite3.Database }))
+            .then(db => {
+                db.migrate({
+                    migrationsPath: path.resolve(__dirname, '../../migrations'),
+                    // force: process.env.NODE_ENV === 'development' ? 'last' : false
+                    force: false
+                });
+                return db;
+            })
+            .catch(err => console.error(err));
+    }
 };
