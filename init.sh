3b2c1098fab383cd03a8bc76133909db1d438032
Michael Lam <michael@jasworks.org>
2021-02-13 13:00:37 +0800

    Changed Dockerfile to allow plugins to be installed and configurable. Added a hook to allow plugin to be installed.

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Use npm install @schnack/plugin-auth-github to install additional plugins. Plugin list can be found here:
+# https://github.com/schn4ck/schnack-plugins
+
+source config/plugins
+
+
+npm run server
