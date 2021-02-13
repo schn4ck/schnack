7c6472df52532902ad20523c3acec20a4fb8ae95
Michael Lam <michael@jasworks.org>
2021-02-13 21:34:15 +0800

    Add configuration example, modified init.sh to allow environment PUID to be passed (so that node.js runs as that user.

--- init.sh
+++ init.sh
@@ -5,8 +5,23 @@
 
 if [ -f config/plugins ]
 then
+  echo "Installing plugins"
 	source config/plugins
+  echo "Done installing plugins"
 fi
 
-
-npm run server
+if [ "x$PUID" = "x" ]
+then
+  echo "Run server"
+  npm run server
+else
+  echo "Creating user $PUID"
+  useradd -u $PUID -U -d /usr/src/app appuser
+  if [ $? -ne 0 ]
+  then
+    echo "Cannot create user"
+    exit 1
+  else
+    su - appuser -c "cd /usr/src/app && npm run server"
+  fi
+fi
